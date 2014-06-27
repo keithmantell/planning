71be68a3a3b3165357f4f6886c32ccf04c1e8205
Keith Mantell <keithmantell@gmail.com>
2014-06-27 23:40:39 +0100

    Updated dependencies

--- csv2/project.clj
+++ csv2/project.clj
@@ -1,8 +1,8 @@
 (defproject csv2 "0.0.1-SNAPSHOT"
   :description "Cool new project to do things and stuff"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [csv-map "0.1.0-SNAPSHOT"]
-                 [org.clojure/java.jdbc "0.0.6"]
+                 [csv-map "0.1.0"]
+                 [org.clojure/java.jdbc "0.3.3"]
                  [org.xerial/sqlite-jdbc "3.7.2"]]
-  :profiles {:dev {:dependencies [[midje "1.6.0"]]}})
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
   
